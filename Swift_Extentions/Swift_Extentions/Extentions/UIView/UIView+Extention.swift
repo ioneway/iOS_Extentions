10c3b2dd8e51b52e747a50a5a5f24822d6397da2
oneway <373850874@qq.com>
2016-03-26 13:29:32 +0800

    添加通过UIView获取其Viewcontroller功能

--- Swift_Extentions/Swift_Extentions/Extentions/UIView/UIView+Extention.swift
+++ Swift_Extentions/Swift_Extentions/Extentions/UIView/UIView+Extention.swift
@@ -48,4 +48,32 @@ extension UIView{
             currentSubView .subViewsTree(indentation: indentation+1);
         }
     }
+    
+    
+    // http://stackoverflow.com/a/34615469/3085137
+    private static func firstAvailableUIViewController(fromResponder responder: UIResponder) -> UIViewController? {
+        func traverseResponderChainForUIViewController(responder: UIResponder) -> UIViewController? {
+            if let nextResponder = responder.nextResponder() {
+                if let nextResp = nextResponder as? UIViewController {
+                    return nextResp
+                } else {
+                    return traverseResponderChainForUIViewController(nextResponder)
+                }
+            }
+            return nil
+        }
+        
+        return traverseResponderChainForUIViewController(responder)
+    }
+    
+    /**
+     获取当前view的viewcontroller
+     
+     - returns: 返回当前view的viewcontroller
+     */
+    var viewController: UIViewController? {
+        get{
+            return UIView.firstAvailableUIViewController(fromResponder: self as UIResponder);
+        }
+    }
 }
